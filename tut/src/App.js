cdf8e68e2ee3636f9b2d657c941de616a1f905c0
velvet-jedi <pranjalruhela2811@gmail.com>
2024-06-04 20:02:35 +0530

    01

---
+++ tut/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+
+  const name = 'Dave';
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
