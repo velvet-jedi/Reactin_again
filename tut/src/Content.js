3f8b7cfe3fbda055cccf30f4fe7b540aa5e4ccb8
velvet-jedi <pranjalruhela2811@gmail.com>
2024-06-04 21:27:54 +0530

    click events

--- tut/src/Content.js
+++ tut/src/Content.js
@@ -8,10 +8,23 @@ const Content = () => {
         return names[int]
       }
 
+      const handleClick = () => {
+        console.log("You clcked");
+      }
+      const handleClick2 = (name) => {
+        console.log(`${name} was clicked`);
+      }
+      const handleClick3 = (e) => {
+        console.log(e.target.innerText);
+      }
+
   return (
     <main>
         
-        <p>Hello {handleNameChange()}</p>
+        <p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
+        <button onClick={handleClick}>Click it</button>
+        <button onClick={() => handleClick2('Dave')}>Click it</button>
+        <button onClick={(e) => handleClick3(e)}>Click it</button>
       
     </main>
   )
