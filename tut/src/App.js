793e34aae9b347ce645f84c90e04af0504aa6d58
velvet-jedi <pranjalruhela2811@gmail.com>
2024-06-04 20:10:32 +0530

    JSX expressions

--- tut/src/App.js
+++ tut/src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 
 function App() {
 
-  const name = 'Dave';
-
+  const handleNameChange = () => {
+    const names = ['John', 'Jane', 'Dave'];
+    const int = Math.floor(Math.random() * 3)    
+    return names[int]
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-
+        <p>Hello {handleNameChange()}</p>
       </header>
+      
     </div>
   );
 }
