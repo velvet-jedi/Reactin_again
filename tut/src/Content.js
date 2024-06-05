0e1a5c76b39082799dee2978f16f345fd04a0f3f
velvet-jedi <pranjalruhela2811@gmail.com>
2024-06-05 11:54:51 +0530

    change state on click- used reference function

--- tut/src/Content.js
+++ tut/src/Content.js
@@ -7,7 +7,8 @@ const Content = () => {
     const handleNameChange = () => {
         const names = ['John', 'Jane', 'Dave'];
         const int = Math.floor(Math.random() * 3)    
-        return names[int]
+        // return names[int]
+        setName(names[int])
       }
 
       const handleClick = () => {
@@ -26,7 +27,7 @@ const Content = () => {
         <p onDoubleClick={handleClick}>Hello 
         {/* {handleNameChange()}</p> */}
         {name}</p>
-        <button onClick={handleClick}>Click it</button>
+        <button onClick={handleNameChange}>Change Name</button>
         <button onClick={() => handleClick2('Dave')}>Click it</button>
         <button onClick={(e) => handleClick3(e)}>Click it</button>
       
