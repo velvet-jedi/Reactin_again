2d738e185996eb3db13e97dbd164abd6557f9cb6
velvet-jedi <pranjalruhela2811@gmail.com>
2024-06-05 13:06:40 +0530

    map over the array

--- tut/src/Content.js
+++ tut/src/Content.js
@@ -2,35 +2,64 @@ import { useState } from 'react';
 import React from 'react'
 
 const Content = () => {
-  const [name, setName] = useState('John') // default state willbe John
+  // const [name, setName] = useState('John') // default state willbe John
 
-    const handleNameChange = () => {
-        const names = ['John', 'Jane', 'Dave'];
-        const int = Math.floor(Math.random() * 3)    
-        // return names[int]
-        setName(names[int])
-      }
+    // const handleNameChange = () => {
+    //     const names = ['John', 'Jane', 'Dave'];
+    //     const int = Math.floor(Math.random() * 3)    
+    //     // return names[int]
+    //     setName(names[int])
+    //   }
 
-      const handleClick = () => {
-        console.log("You clcked");
-      }
-      const handleClick2 = (name) => {
-        console.log(`${name} was clicked`);
-      }
-      const handleClick3 = (e) => {
-        console.log(e.target.innerText);
-      }
+    //   const handleClick = () => {
+    //     console.log("You clcked");
+    //   }
+    //   const handleClick2 = (name) => {
+    //     console.log(`${name} was clicked`);
+    //   }
+      // const handleClick3 = (e) => {
+      //   console.log(e.target.innerText);
+      // }
+
+      const [items, setItems] = useState([
+        {
+          id: 1,
+          read: false,
+          item: 'Gravity'
+        },
+        {
+          id: 2,
+          read: false,
+          item: 'Telescopes'
+        },
+        {
+          id: 3,
+          read: false,
+          item: 'Electromagnetism'
+        }
+      ]);
 
   return (
     <main>
         
-        <p onDoubleClick={handleClick}>Hello 
+        {/* <p onDoubleClick={handleClick}>Hello  */}
         {/* {handleNameChange()}</p> */}
-        {name}</p>
+        {/* {name}</p>
         <button onClick={handleNameChange}>Change Name</button>
         <button onClick={() => handleClick2('Dave')}>Click it</button>
-        <button onClick={(e) => handleClick3(e)}>Click it</button>
-      
+        <button onClick={(e) => handleClick3(e)}>Click it</button> */}
+      <ul>
+        {items.map((item) => (
+          <li className='item' style= { { listStyle: 'none' } }>
+            <input 
+              type="checkbox"
+              read={item.read} 
+              />
+              <label>{item.item}</label>
+              <button>Delete</button>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
