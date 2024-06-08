ff87b5f70a7483ee6a6e7c65653b882593260c65
velvet-jedi <pranjalruhela2811@gmail.com>
2024-06-08 17:42:04 +0530

    shiftfocus with useRef back to input after adding an item

--- tut/src/AddItem.js
+++ tut/src/AddItem.js
@@ -1,8 +1,13 @@
 // form component to add new items to list
+
 import React from 'react'
 import { FaPlus } from 'react-icons/fa'
+import { useRef } from 'react';
 
 const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+
+  const inputRef = useRef();
+  
   return (
     <form className='addForm' onSubmit={handleSubmit}>
         <label htmlFor="addItem">Add a new book</label>
@@ -10,6 +15,7 @@ const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
             type="text" 
             id='addItem'
             autoFocus
+            ref={inputRef} // apply the ref to the input
             placeholder='Add item'
             required    /**dont let ths user submit the form without this input basically dont let the user add a new item withput fillin ghte input */
             value={newItem} // set the value of the input to the state (our one source of truth)
@@ -18,6 +24,7 @@ const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
         <button 
             type='submit'
             aria-label='Add Item'
+            onClick={() => inputRef.current.focus()}
         >
             <FaPlus />
         </button> 
