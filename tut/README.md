801f7f3b8539b1024b90027fce95ab5a27dbe7fd
velvet-jedi <pranjalruhela2811@gmail.com>
2024-06-11 14:06:50 +0530

    fetch API and setItems state on initial loading

--- tut/README.md
+++ tut/README.md
@@ -1,11 +1,17 @@
-- JSON server for the backend
+- JSON server for the backend REST API hosted on /items endpoint
 
 Starting the JSON server to mimic the backend
 ```
 npx json-server -p 3500 -w data/db.json
 ```
 
+- Fetching data from API
+- Using functional components
+- Click events
+- Key attribute
+- Props
+- Forms
 - localstorage
 - useEffect
 - useState
-
+- async / await for promise resolution
--- tut/src/App.js
+++ tut/src/App.js
@@ -8,8 +8,12 @@ import { useState, useEffect } from 'react';
 
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('readingList')) || []); // get items from local storage
-  
+  const API_URL = 'http://localhost:3500/items';
+  // const [items, setItems] = useState(JSON.parse(localStorage.getItem('readingList')) || []); // get items from local storage
+
+  const [items, setItems] = useState([]); // initially set this items state to be an empty array
+
+
   //   [
   //     {
   //     id: 1,
@@ -36,10 +40,22 @@ function App() {
   const [search, setSearch] = useState(''); 
 
   
-  useEffect(() => {
-    localStorage.setItem('readingList', JSON.stringify(items));
-  }, [items])   // real time change saved to local storage
+  // useEffect(() => {
+  //   localStorage.setItem('readingList', JSON.stringify(items));
+  // }, [items])   // real time change saved to local storage
 
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL); // get the response
+        const listItems = await response.json();  // parse the response
+        setItems(listItems); // set the state to what we recieved from the API
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    fetchItems()
+  }, []) // only happens at load time
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
